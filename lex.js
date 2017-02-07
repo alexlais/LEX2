793100b3710e9ab3b4b919f0ce5e1f893007d6fb
Alex L <alexlais@mail.com>
2017-02-07 00:09:43 -0500

    design and color changes

--- lex.js
+++ lex.js
@@ -3,9 +3,6 @@
  * @author Nick Kaye <nick.c.kaye@gmail.com>
  */
 
-/**
- * Main AngularJS Web Application
- */
 
 var app = angular.module('WebApp', ['ngRoute']);
 
@@ -30,6 +27,22 @@ var app = angular.module('WebApp', ['ngRoute']);
         .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
     }]);
 
+    // NEW CODE -------------
+//     .controller('Controller', ['$scope', function($scope) {
+//       $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
+//       $scope.igor = { name: 'Igor', address: '123 Somewhere' };
+//     }])
+//     .directive('myCustomer', function() {
+//       return {
+//         restrict: 'E',
+//         scope: {
+//           customerInfo: '=info'
+//         },
+//         templateUrl: 'my-customer-iso.html'
+//       };
+//     });
+
+    // OLD CODE -----------
 	app.controller("teamHomeCtrl", function ($scope) {
 		var countries = [
             {countryName: "USA",
