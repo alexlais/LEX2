65075eba051865aa85704e665505a7732e9e62c6
Alex L <alexlais@mail.com>
2017-02-09 17:43:34 -0500

    profile list

--- lex.js
+++ lex.js
@@ -3,50 +3,204 @@
  * @author Nick Kaye <nick.c.kaye@gmail.com>
  */
 
+var app =
+    angular.module('WebApp', [
+    'ngRoute',
+    'ngAnimate'
+]);
 
-var app = angular.module('WebApp', ['ngRoute']);
+(function(angular) {
+  'use strict';
 
     // Configure the Routes
     app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
         // Pages
-        .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
-        .when("/the_firm", {templateUrl: "partials/the_firm.html", controller: "PageCtrl"})
-        .when("/practice_areas", {templateUrl: "partials/practice_areas.html", controller: "PageCtrl"})
-        .when("/our_team", {templateUrl: "partials/our_team.html", controller: "ourteamCtrl"})
-        .when("/our_team/Lucius_Smejda", {templateUrl: "partials/our_team/Lucius_Smejda.html", controller: "teamHomeCtrl"})
-        .when("/our_team/Joseph_McFarland", {templateUrl: "partials/our_team/Joseph_McFarland.html", controller: "teamHomeCtrl"})
-        .when("/our_team/Antonio_Zamora", {templateUrl: "partials/our_team/Antonio_Zamora.html", controller: "teamHomeCtrl"})
-        .when("/our_team/Elke_Rolff", {templateUrl: "partials/our_team/Elke_Rolff.html", controller: "teamHomeCtrl"})
-        .when("/our_team/Maxim_Istomin", {templateUrl: "partials/our_team/Maxim_Istomin.html", controller: "teamHomeCtrl"})
-        .when("/our_team/Susanne_Leone", {templateUrl: "partials/our_team/Susanne_Leone.html", controller: "teamHomeCtrl"})
-        .when("/our_team/Evgeny_Golovanov", {templateUrl: "partials/our_team/Evgeny_Golovanov.html", controller: "teamHomeCtrl"})
+        .when("/", {
+        title : 'Transaction, Business and Investment Legal Counseling Advice',
+		robots : 'follow,index',
+		description : 'An international law firm, focused on the jurisdictions of Europe and the Americas. We provide legal planning, advisory, counseling, and support services to large or small enterprises, start-up businesses, investors, and entrepreneurs engaged in international transactions, business or investments world-wide.',
+		keywords : 'international law firm, international transaction, international business, international investments',
+		canonical : '',
+        templateUrl : 'partials/home.html',
+        controller  : 'PageCtrl',
+        navStyle : 'noscrollActive noscroll'
+        })
+        .when("/the_firm", {
+        title : 'Our Firm Values: Quality, Teamwork, Confidentiality, Strong Relationships',
+		robots : 'follow,index',
+		description : 'An international law firm, focused on the jurisdictions of Europe and the Americas. We provide legal planning, advisory, counseling, and support services to large or small enterprises, start-up businesses, investors, and entrepreneurs engaged in international transactions, business or investments world-wide.',
+		keywords : 'international law firm, international transaction, international business, international investments',
+		canonical : '',
+        templateUrl: "partials/the_firm.html",
+        controller: "PageCtrl",
+        navStyle : 'noscrollActive noscroll'
+        })
+        .when("/practice_areas", {
+        title : 'Our Practice Areas, International Transaction, Business and Investment',
+		robots : 'follow,index',
+		description : 'An international law firm, focused on the jurisdictions of Europe and the Americas. We provide legal planning, advisory, counseling, and support services to large or small enterprises, start-up businesses, investors, and entrepreneurs engaged in international transactions, business or investments world-wide.',
+		keywords : 'international law firm, international transaction, international business, international investments',
+		canonical : '',
+        templateUrl: "partials/practice_areas.html",
+        controller: "PageCtrl",
+        navStyle : 'noscrollActive noscroll'
+        })
+        .when("/our_team", {
+        title : 'Our Team. Teamwork and Highly Qualified Professionals is Our Aproach',
+		robots : 'follow,index',
+		description : 'An international law firm, focused on the jurisdictions of Europe and the Americas. We provide legal planning, advisory, counseling, and support services to large or small enterprises, start-up businesses, investors, and entrepreneurs engaged in international transactions, business or investments world-wide.',
+		keywords : 'international law firm, international transaction, international business, international investments',
+		canonical : '',
+        templateUrl: "partials/our_team.html",
+        controller: "ourteamCtrl",
+        navStyle : 'noscrollActive noscroll'
+        })
+        .when("/test", {
+        templateUrl: "partials/test.html",
+        controller: "ourteamCtrl",
+        navStyle : 'noscrollActive noscroll'
+        })
+        .when("/our_team/test", {
+        templateUrl: "partials/our_team/test.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Joseph_McFarland", {
+        title : 'Joseph McFarland - Our Team',
+        templateUrl: "partials/our_team/Lucius_Smejda.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Joseph_McFarland", {
+        title : 'Joseph McFarland - Our Team',
+        templateUrl: "partials/our_team/Joseph_McFarland.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Antonio_Zamora", {
+        title : 'Antonio Zamora - Our Team',
+        templateUrl: "partials/our_team/Antonio_Zamora.html",
+        controller: "teamHomeCtrl",
+        })
+        .when("/our_team/Elke_Rolff", {
+        title : 'Elke Rolff - Our Team',
+        templateUrl: "partials/our_team/Elke_Rolff.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Maxim_Istomin", {
+        title : 'Maxim Istomin - Our Team',
+        templateUrl: "partials/our_team/Maxim_Istomin.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Susanne_Leone", {
+        title : 'Susanne Leone - Our Team',
+        templateUrl: "partials/our_team/Susanne_Leone.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Evgeny_Golovanov", {
+        title : 'Evgeny Golovanov - Our Team',
+        templateUrl: "partials/our_team/Evgeny_Golovanov.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Jasmin_Welker", {
+        title : 'Jasmin Welker - Our Team',
+        templateUrl: "partials/our_team/Jasmin_Welker.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Jacques_Ethier", {
+        title : 'Jacques Ethier - Our Team',
+        templateUrl: "partials/our_team/Jacques_Ethier.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Eric_Citrey", {
+        title : 'Eric Citrey - Our Team',
+        templateUrl: "partials/our_team/Eric_Citrey.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Oliver_Hance", {
+        title : 'Oliver Hance - Our Team',
+        templateUrl: "partials/our_team/Oliver_Hance.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Aleksandr_Smirnov", {
+        title : 'Aleksandr Smirnov - Our Team',
+        templateUrl: "partials/our_team/Aleksandr_Smirnov.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Vladimir_Troitskiy", {
+        title : 'Vladimir Troitskiy - Our Team',
+        templateUrl: "partials/our_team/Vladimir_Troitskiy.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Carlos_Aud_Sobrinho", {
+        title : 'Carlos Aud Sobrinho - Our Team',
+        templateUrl: "partials/our_team/Carlos_Aud_Sobrinho.html",
+        controller: "teamHomeCtrl"
+        })
+        .when("/our_team/Eric_Vigneron", {
+        title : 'Eric Vigneron - Our Team',
+        templateUrl: "partials/our_team/Eric_Vigneron.html",
+        controller: "teamHomeCtrl"
+        })
      // .when("/our_team/:memberName", {templateUrl: "partials/our_team_member.html", controller: "ourteamCtrl"})
-        .when("/links_events", {templateUrl: "partials/links_events.html", controller: "PageCtrl"})
-        .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-        .when("/not_found", {templateUrl: "partials/not_found.html", controller: "PageCtrl"})
+        .when("/contact", {
+        templateUrl: "partials/contact.html",
+        controller: "PageCtrl",
+        navStyle : 'noscrollActive noscroll'
+        })
+        .when("/links_events", {
+        templateUrl: "partials/links_events.html",
+        controller: "PageCtrl",
+        navStyle : 'noscrollActive noscroll'
+        })
+        .when("/not_found", {
+        templateUrl: "partials/not_found.html",
+        controller: "PageCtrl"
+        })
 
         // else 404
         .otherwise("/not_found", {redirectTo: "partials/not_found.html", controller: "PageCtrl"});
+
+        // $locationProvider.html5Mode(true);
     }]);
 
-    // NEW CODE -------------
-//     .controller('Controller', ['$scope', function($scope) {
-//       $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
-//       $scope.igor = { name: 'Igor', address: '123 Somewhere' };
-//     }])
-//     .directive('myCustomer', function() {
-//       return {
-//         restrict: 'E',
-//         scope: {
-//           customerInfo: '=info'
-//         },
-//         templateUrl: 'my-customer-iso.html'
-//       };
+//     $rootScope.$on("$routeChangeSuccess", function (event, currentRoute, previousRoute) {
+//         window.scrollTo(0, 0);
 //     });
 
+	app.run(['$rootScope', function ($rootScope) {
+		$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+			$rootScope.title = current.$$route.title;
+			$rootScope.description = current.$$route.description;
+			$rootScope.keywords = current.$$route.keywords;
+			$rootScope.canonical = current.$$route.canonical;
+			$rootScope.robots = current.$$route.robots;
+			$rootScope.navStyle = current.$$route.navStyle;
+			// angular.element(document.getElementsByTagName('head')).append(angular.element('<base href="' + window.location.pathname + '" />'));
+            // alert (window.location.pathname);
+
+		});
+	 }]);
+
+    // NEW CODE -------------
+
+
+//         .controller('Controller', ['$scope', function($scope) {
+//           $scope.naomi = { name: 'Naomi', address: '1600 Amphitheatre' };
+//           $scope.igor = { name: 'Igor', address: '123 Somewhere' };
+//         }])
+//         .directive('myCustomer', function() {
+//           return {
+//             restrict: 'E',
+//             scope: {
+//               customerInfo: '=info'
+//             },
+//             templateUrl: 'my-customer-iso.html'
+//           };
+//         });
+
+
+
     // OLD CODE -----------
-	app.controller("ourteamCtrl", ['$scope', function ($scope, $routeParams) {
+	app.controller('ourteamCtrl', ['$scope', function ($scope, $routeParams) {
 		$scope.countries = [
             {countryName: "USA",
                 teamMember: [
@@ -84,7 +238,12 @@ var app = angular.module('WebApp', ['ngRoute']);
         	        memberName: "Evgeny V. Golovanov",
         	        memberTitle: "Jurist",
         	        memberURL: "Evgeny_Golovanov",
-        	        imageExt: "jpg"}
+        	        imageExt: "jpg"},
+        	        {
+        	        memberName: "Jasmin Welker",
+        	        memberTitle: "Jurist",
+        	        memberURL: "Jasmin_Welker",
+        	        imageExt: "png"}
 		        ]
             },
             {countryName: "International",
@@ -117,12 +276,12 @@ var app = angular.module('WebApp', ['ngRoute']);
         	        {
         	        memberName: "Carlos Aud Sobrinho",
         	        memberTitle: "Brazil",
-        	        memberURL: "Carlos_AUD_SOBRINHO",
+        	        memberURL: "Carlos_Aud_Sobrinho",
         	        imageExt: "jpg"},
         	        {
         	        memberName: "Eric Vigneron",
         	        memberTitle: "Dominican Republic",
-        	        memberURL: "Eric_VIGNERON",
+        	        memberURL: "Eric_Vigneron",
         	        imageExt: "png"}
 		        ]
 		    } //END international
@@ -132,34 +291,22 @@ var app = angular.module('WebApp', ['ngRoute']);
 	} // END function
 	]);
 
-//     $rootScope.$on("$routeChangeSuccess", function (event, currentRoute, previousRoute) {
-//         window.scrollTo(0, 0);
-//     });
 
-//
-// /**
-//  * Controls the Blog
-//  */
-// app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-//   console.log("Blog Controller reporting for duty.");
-// });
-//
-// /**
-//  * Controls all other Pages
-//  */
-// app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-//   console.log("Page Controller reporting for duty.");
-//
-//   // Activates the Carousel
-//   $('.carousel').carousel({
-//     interval: 5000
-//   });
-//
-//   // Activates Tooltips for Social Links
-//   $('.tooltip-social').tooltip({
-//     selector: "a[data-toggle=tooltip]"
-//   })
-// });
+
+  app.controller('AppCtrl', ['$scope', '$interval', function($scope, $interval) {
+    $scope.number = 0;
+    $interval(function() {
+      $scope.number++;
+    }, 1000);
+
+    var colors = ['red','blue','green','yellow','orange'];
+    $scope.colorClass = function(number) {
+      return colors[number % colors.length];
+    };
+  }]);
+
+
+
 
 
 
@@ -168,9 +315,9 @@ var app = angular.module('WebApp', ['ngRoute']);
 $(window).on('scroll', function(){
     var slideHeight = ('50');
 	if( $(window).scrollTop()>slideHeight){
-		$('.navbar').removeClass('noscroll');
+		$('.navbar.noscrollActive').removeClass('noscroll');
 	} else {
-		$('.navbar').addClass('noscroll');
+		$('.navbar.noscrollActive').addClass('noscroll');
 	}
 });
 
@@ -183,3 +330,4 @@ $('.dropdown-toggle').click(function() {
 
 
 
+})(window.angular);
